b0b081050982cb50a0c2129f2c18498dc1479bc0
CORE jake <corejake609@gmail.com>
2026-02-01 08:09:48 +0300

    feat: allow for specification of keycode triggers

--- niri-config/src/binds.rs
+++ niri-config/src/binds.rs
@@ -10,6 +10,7 @@ use niri_ipc::{
     ColumnDisplay, LayoutSwitchTarget, PositionChange, SizeChange, WorkspaceReferenceArg,
     ZoomLevelChange,
 };
+use smithay::backend::input::Keycode;
 use smithay::input::keyboard::keysyms::KEY_NoSymbol;
 use smithay::input::keyboard::xkb::{keysym_from_name, KEYSYM_CASE_INSENSITIVE, KEYSYM_NO_FLAGS};
 use smithay::input::keyboard::Keysym;
@@ -40,6 +41,7 @@ pub struct Key {
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub enum Trigger {
     Keysym(Keysym),
+    Keycode(Keycode),
     MouseLeft,
     MouseRight,
     MouseMiddle,
@@ -1017,6 +1019,11 @@ impl FromStr for Key {
             Trigger::TabletStylusButton2
         } else if key.eq_ignore_ascii_case("TabletStylusButton3") {
             Trigger::TabletStylusButton3
+        } else if let Some(num) = key.strip_prefix("keycode:") {
+            let Ok(keycode) = num.parse::<u32>() else {
+                return Err(miette!("invalid keycode: {key}"));
+            };
+            Trigger::Keycode(keycode.into())
         } else {
             let mut keysym = keysym_from_name(key, KEYSYM_CASE_INSENSITIVE);
             // The keyboard event handling code can receive either
